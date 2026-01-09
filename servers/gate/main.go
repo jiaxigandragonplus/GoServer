7e1031544cfb6c15397aaa346584c547855bd00a
jiaxigandragonplus <jiaxi.gan@dragonplus.com>
2026-01-09 11:52:26 +0800

    增加配置读取测试

--- servers/gate/main.go
+++ servers/gate/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"time"
 
+	"github.com/GooLuck/GoServer/framework/config"
 	"github.com/GooLuck/GoServer/framework/logger"
 )
 
 func main() {
 	initLogger()
+	initConfig()
 
 	logger.Info("gate start", "timestamp", time.Now().Unix())
 }
@@ -16,4 +18,23 @@ func initLogger() {
 	// 初始化日志
 	logger.InitDefaultLogger("gate", logger.DebugLevel)
 	logger.Debug("this is debug log")
+}
+
+type ClusterConfig struct {
+	ClusterName string `json:"clusterName"`
+}
+
+func initConfig() {
+	v, err := config.GetConfig("gate")
+	if err != nil {
+		panic(err)
+	}
+
+	clusterConfig := new(ClusterConfig)
+	err = v.Unmarshal(clusterConfig)
+	if err != nil {
+		panic(err)
+	}
+
+	logger.Info("init config", "cluster info", clusterConfig)
 }
